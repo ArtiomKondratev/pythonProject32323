51cd0a8573604e089cc466461349b03447ae83e9
ArtiomKondratev <artem-kondratev-1980@mail.ru>
2022-05-02 23:18:42 +0300

    jnacjlanck

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
 import socket
 import requests
 import re
-# import ipaddress
-# import time
+import ipaddress
+import time
 from requests.packages.urllib3.exceptions import InsecureRequestWarning
-# import threading
+import threading
 
 requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
 
 class ScannerSN:
-    host = ''
-    mask = ''
+    # Код Стаса
+    host = None
+    mask = None
     ports_to_scan = []
+    max_threading = 100
 
     def __init__(self):
         self.get_input_data()
@@ -32,46 +34,85 @@ class ScannerSN:
             return
 
     def get_ip(self):
-        choice = input('Если у вас есть IP - введите {IP}. Если у вас есть доменное имя - введите DI')
+        choice = input('Если у вас есть IP - введите {IP}. Если у вас есть доменное имя - введите {DI}: ')
         if choice == 'IP':
             ip = input('Введите сетевой адресс и сетевой префикс в  формате 127.0.0.1/24\n').replace(' ', '')
             if len(ip) > 0:
-                check_ip = re.findall(r'([0-9]{1,3}\.[0-9]{1,3}]\.[0-9]{1,3}\.[0-9]{1,3})/([0-9]{1,3})]', ip)
+                check_ip = re.findall(r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/(\d{1,3})", ip)
                 if len(check_ip) > 0:
-                    print(check_ip)
                     self.host = check_ip[0][0]
                     self.mask = check_ip[0][1]
                     return self.host, self.mask
             print('No ip')
         elif choice == 'DI':
-            domain = input('Введите домен')
+            domain = input('Введите домен: ')
             ip = socket.gethostbyname(domain)
-            mask = 'Введите маску'
+            print(ip)
+            mask = input('Введите маску: ')
+            ip = ip + "/" + mask
             if len(ip) > 0 and len(mask) > 0:
-                check_ip = re.findall(r'([0-9]{1,3}\.[0-9]{1,3}]\.[0-9]{1,3}\.[0-9]{1,3})]', ip)
+                check_ip = re.findall(r"(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})/(\d{1,3})", ip)
                 if len(check_ip) > 0:
-                    self.host = check_ip
-                    self.mask = mask
+                    self.host = check_ip[0][0]
+                    self.mask = check_ip[0][1]
                     return self.host, self.mask
             print('No domain or mask')
 
     def get_ports(self):
-        ports = input('Введите через пробел порты, которые нужно просканировать')
+        ports = input('Введите через пробел порты, которые нужно просканировать: ')
         if len(ports) > 0:
             self.ports_to_scan = list(set([int(port) for port in re.findall('[0-9]+', ports)]))
             if len(self.ports_to_scan) > 0:
                 return self.ports_to_scan
         print('No correct ports or no ports at all')
 
+    def start_cumming(self):
+        if len(self.host) > 0 and len(self.ports_to_scan) > 0 and len(self.mask) > 0:
+            print('Проверка запущена...')
+            network = ipaddress.ip_network(f'{self.host}/{self.mask}', strict=False)
+            network_address = network.network_address
+            broadcast_address = network.broadcast_address
+
+            if network_address is not None:
+                self.scan_ports(ip=f'{network_address}', ports=self.ports_to_scan)
+
+            if broadcast_address is not None and broadcast_address != network_address:
+                self.scan_ports(ip=f'{network_address}', ports=self.ports_to_scan)
+
+            for host in list(network.hosts()):
+                if str(f'{host}') != str(network_address) and str(f'{host}') != str(broadcast_address):
+                    while threading.active_count() > self.max_threading:
+                        time.sleep(1)
+                    self.scan_ports(ip=f'{host}', ports=self.ports_to_scan)
+
+    def scan_ports(self, ip, ports):
+        for port in ports:
+            self.check_connection(ip=ip, port=port)
+
+    def check_connection(self, ip, port):
+        sock = socket.socket()
+        try:
+            sock.settimeout(5.0)
+            sock.connect((ip, port))
+        except Exception as err:
+            print(err)
+        else:
+            if port == 443 or port == 80:
+                msg = self.get_service_name(host=ip, port=port)
+                print(f'{ip} {port} OPEN {msg}')
+            else:
+                print(f'{ip} {port} OPEN')
+        finally:
+            sock.close()
+
     @staticmethod
-    def get_service_name(s, port1, protocol1='http'):
-        host = socket.gethostbyname(f'{s}')
-        if port1 == 443:
+    def get_service_name(host, port, protocol1='http'):
+        if port == 443:
             protocol1 = 'https'
-        elif port1 == 80:
+        elif port == 80:
             protocol1 = 'http'
         try:
-            response = requests.get(f'{protocol1}://{host}:{port1}', verify=False)
+            response = requests.get(f'{protocol1}://{host}:{port}', verify=False)
             header = response.headers
             if 'server' in header.keys():
                 service_name = response.headers.get('server')
@@ -79,8 +120,8 @@ class ScannerSN:
             elif 'Server' in header.keys():
                 service_name = response.headers.get('server')
                 return f'{service_name}'
-        except Exception:
-            pass
+        except Exception as err:
+            print(err)
 
 
 class NedoScannerSN:
@@ -129,8 +170,8 @@ class NedoScannerSN:
             sock.settimeout(10)
             try:
                 return sock.connect_ex((f'{s}', port))
-            except Exception:
-                pass
+            except Exception as err:
+                print(err)
 
     def output(self):
         s = self.get_input_data()
--- start.py
+++ start.py
@@ -1,6 +1,14 @@
-from main import NedoScannerSN
+from main import NedoScannerSN, ScannerSN
+choice = input("Выберите метод. Scanner или NedoScanner) ")
+if choice == "Scanner":
+    try:
+        port_scanner = ScannerSN()
+        port_scanner.start_cumming()
+    except KeyboardInterrupt:
+        pass
 
-try:
-    port_scanner = NedoScannerSN()
-except KeyboardInterrupt:
-    pass
+elif choice == "NedoScanner":
+    try:
+        port_scanner = NedoScannerSN()
+    except KeyboardInterrupt:
+        pass
